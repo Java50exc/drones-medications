6d645978431b017cffdd0ea56dbf67c1d9f5f4a7
Yuragranovsky <yuragranovsky@gmail.com>
2024-02-01 17:17:52 +0200

    adds test cases in DronesServiceTest for check methods

--- src/test/java/telran/drones/DronesServiceTest.java
+++ src/test/java/telran/drones/DronesServiceTest.java
@@ -1,5 +1,4 @@
 package telran.drones;
-import telran.drones.api.*;
 import telran.drones.dto.*;
 import telran.drones.model.*;
 import telran.drones.exceptions.*;
@@ -10,21 +9,28 @@ import telran.drones.service.DronesService;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 @SpringBootTest
 @Sql(scripts = "classpath:test_data.sql")
 
 class DronesServiceTest {
 	private static final String DRONE1 = "Drone-1";
+	private static final String DRONE2 = "Drone-2";
 	private static final String MED1 = "MED_1";
 	private static final String DRONE3 = "Drone-3";
 	private static final String SERVICE_TEST = "Service: ";
 	private static final String DRONE4 = "Drone-4";
+	private static final String MED2 = "MED_2";
 	@Autowired
  DronesService dronesService;
 	@Autowired
@@ -33,7 +39,8 @@ class DronesServiceTest {
 	EventLogRepo logRepo;
 	DroneDto droneDto = new DroneDto(DRONE4, ModelType.Cruiserweight);
 	DroneDto drone1 = new DroneDto(DRONE1, ModelType.Middleweight);
-	DroneMedication droneMedication1 = new DroneMedication(DRONE1, MED1);		
+	DroneMedication droneMedication1 = new DroneMedication(DRONE1, MED1);
+	DroneMedication droneMedication2 = new DroneMedication(DRONE2, MED2);		
 			
 	@Test
 	@DisplayName(SERVICE_TEST + TestDisplayNames.LOAD_DRONE_NORMAL)
@@ -81,6 +88,54 @@ class DronesServiceTest {
 	void registerDroneAlreadyExists() {
 		assertThrowsExactly(DroneAlreadyExistException.class,
 				() -> dronesService.registerDrone(drone1));
+	}
+	@Test
+	@DisplayName(SERVICE_TEST + TestDisplayNames.CHECK_MED_ITEMS_NORMAL)
+	void checkDroneMedItemsNormal() {
+		dronesService.loadDrone(droneMedication1);
+		List<String> medItemsExpected = List.of(MED1);
+		List<String> medItemsActual = dronesService.checkMedicationItems(DRONE1);
+		assertIterableEquals(medItemsExpected, medItemsActual);
+		assertTrue(dronesService.checkMedicationItems(DRONE2).isEmpty());
+	}
+	@Test
+	@DisplayName(SERVICE_TEST + TestDisplayNames.CHECK_MED_ITEMS_DRONE_NOT_FOUND)
+	void checkDroneMedItemsNotFound() {
+		assertThrowsExactly(DroneNotFoundException.class,
+				()->dronesService.checkMedicationItems(DRONE4));
+	}
+	@Test
+	@DisplayName(SERVICE_TEST + TestDisplayNames.AVAILABLE_DRONES)
+	void checkAvailableDrones() {
+		List<String> availableExpected = List.of(DRONE1);
+		List<String> availableActual = dronesService.checkAvailableDrones();
+		assertIterableEquals(availableExpected, availableActual);
+		dronesService.loadDrone(droneMedication1);
+		assertTrue(dronesService.checkAvailableDrones().isEmpty());
+	}
+	@Test
+	@DisplayName(SERVICE_TEST + TestDisplayNames.CHECK_BATTERY_LEVEL_NORMAL)
+	void checkBatteryCapacityNormal() {
+		assertEquals(100, dronesService.checkBatteryCapacity(DRONE1));
+	}
+	@Test
+	@DisplayName(SERVICE_TEST + TestDisplayNames.CHECK_BATTERY_LEVEL_DRONE_NOT_FOUND)
+	void checkBatteryCapacityNotFound() {
+		assertThrowsExactly(DroneNotFoundException.class,
+				()->dronesService.checkBatteryCapacity(DRONE4));
+	}
+	@Test
+	@DisplayName(SERVICE_TEST + TestDisplayNames.CHECK_DRONES_ITEMS_AMOUNT)
+	void checkDroneLoadedItemAmounts() {
+		dronesService.loadDrone(droneMedication1);
+		Map<String, Long> resultMap =
+				dronesService.checkDroneLoadedItemAmounts().stream()
+				.collect(Collectors.toMap(da -> da.getNumber(), da -> da.getAmount()));
+		assertEquals(3, resultMap.size());
+		assertEquals(1, resultMap.get(DRONE1));
+		assertEquals(0, resultMap.get(DRONE2));
+		assertEquals(0, resultMap.get(DRONE3));
+		
 	}
 	
 }
