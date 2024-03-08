ca26e626666aba784ea6a1e7ead5721b5d726800
allanteone <allanteone@gmail.com>
2024-03-08 10:24:49 +0200

    eventLogRepo fix

--- src/main/java/telran/drones/repo/EventLogRepo.java
+++ src/main/java/telran/drones/repo/EventLogRepo.java
@@ -8,16 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import telran.drones.model.EventLog;
 
 public interface EventLogRepo extends JpaRepository<EventLog, Long> {
-	
-	@Query(value="select log from EventLog log where log.droneNumber = :droneNumber order by log.timestamp desc limit 1", nativeQuery=false)
-	EventLog findLastEventLogByDroneNumber(String droneNumber);
-	
 	EventLog findFirstByDroneNumberOrderByTimestampDesc(String droneNumber);
-	
 	int countByDroneNumber(String droneNumber);
-	
 	List<EventLog> findByDroneNumberOrderByTimestampAsc(String droneNumber);
-	
-
 
 }
